1cc9ac7949370d5242feac003cd0c6c26efd0243
shadowcodex <jsduncan98@gmail.com>
2019-02-12 11:32:45 -0600

    added functionTiming and split time parsing to separate function to allow reuse

--- index.js
+++ index.js
@@ -68,6 +68,26 @@ class PTime {
         
     }
 
+    parseTime(diff) {
+        // math
+        let nano = Number(diff);
+        const seconds = Math.floor(nano / 1e9);
+        nano = nano % 1e9;
+        const milliseconds = Math.floor(nano / 1e6);
+        nano = nano % 1e6;
+        
+        // send results
+        return {
+            "nanosecondsDiff": diff,
+            "data": {
+                "seconds": seconds,
+                "milliseconds": milliseconds,
+                "nanoseconds": nano
+            },
+            "formatted": `+ ${seconds}s ${milliseconds}ms ${nano}ns`            
+        };
+    }
+
     /**
      * Gets the difference in time set to time now returning verbose data.
      * @param {bigint|number|string|custom} name the name of the timer to set
@@ -75,28 +95,59 @@ class PTime {
      * @returns {object} an object containing the details of this time difference
      */
     elapsedTime(name) {
-        if(this.validate(name)) {
-            // get time
+        if(this.validate(name)) {            
             const diff = this.diffTime(name);
-            
-            // math
-            let nano = Number(diff);
-            const seconds = Math.floor(nano / 1e9);
-            nano = nano % 1e9;
-            const milliseconds = Math.floor(nano / 1e6);
-            nano = nano % 1e6;
-            
-            // send results
-            return {
-                "nanosecondsDiff": diff,
-                "data": {
-                    "seconds": seconds,
-                    "milliseconds": milliseconds,
-                    "nanoseconds": nano
-                },
-                "formatted": `+ ${seconds}s ${milliseconds}ms ${nano}ns`            
-            };
+            return this.parseTime(diff);            
         }
+    }
+
+    /**
+     * 
+     * @param {string} name the name of this test
+     * @param {function} method the method to test
+     * @param {array} values the values to pass to method, deconstructed as args
+     * @param {int} rounds the number of rounds to test
+     * @param {boolean} sync whether or not function is sync or not.
+     */
+    async runFunctionRounds(name, method, values, rounds, sync) {
+        this.setTime(name)
+        if(sync) {
+            for(let i = 0; i < rounds; i++) {
+                method(...values);
+            }
+        } else {
+            for(let i = 0; i < rounds; i++) {
+                await method(...values);
+            }
+        }
+        const elapsed = this.elapsedTime(name);
+        delete this.timers[name];
+        return elapsed;
+    }
+
+    async runFunctionAverage(name, method, values, rounds, sync) {
+        if(rounds === 0) {
+            return 0;
+        }
+
+        let sum = BigInt(0);
+        for(let i = 0; i < rounds; i++) {
+            if(sync) {
+                ptime.setTime(name+rounds);
+                method(...values);
+                sum += ptime.elapsedTime(name+rounds).nanosecondsDiff;
+                delete ptime.timers[name+rounds];
+            } else {
+                ptime.setTime(name+rounds);
+                await method(...values);
+                sum += ptime.elapsedTime(name+rounds).nanosecondsDiff;
+                delete ptime.timers[name+rounds];
+            }
+            
+        }
+
+        const elapsed = this.parseTime(sum / rounds);
+        return elapsed;
     }
 }
 
